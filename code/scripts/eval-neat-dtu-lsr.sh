8aae0bb1c6622c4aa85b68e69b97523a374a7e33
xn <xuenan199@gmail.com>
2023-04-05 13:18:40 +0800

    add shell scripts

---
+++ code/scripts/eval-neat-dtu-lsr.sh
@@ -0,0 +1,30 @@
+python evaluation/eval-lsr-dtu.py \
+    --scan $1 \
+    --cam ../data/DTU/scan$1/cameras.npz \
+    --data $2
+    
+#scan 16
+# python evaluation/eval-lsr-dtu.py \
+#     --scan 16 \
+#     --cam ../data/DTU/scan16/cameras.npz \
+#     --data ../exps/dtu-rend-d/16/2023_03_05_11_02_20/wireframes/latest-B9f1tiFk-wfi_checked.npz
+
+
+# #scan 17
+# python evaluation/eval-lsr-dtu.py --scan 17 --cam ../data/DTU/scan17/cameras.npz --data ../exps/dtu-rend-d/17/2023_03_05_11_14_56/wireframes/latest-iv3lsN8Z-wfi_checked.npz
+
+# #scan 18
+# python evaluation/eval-lsr-dtu.py --scan 18 --cam ../data/DTU/scan18/cameras.npz --data ../exps/dtu-rend-d/18/2023_03_05_19_28_22/wireframes/latest-efvqCphI-wfi_checked.npz
+
+# #scan 19
+# python evaluation/eval-lsr-dtu.py --scan 19 --cam ../data/DTU/scan19/cameras.npz --data ../exps/dtu-rend-d/19/2023_03_06_11_08_41/wireframes/latest-1EtjKZV3-wfi_checked.npz
+
+# #scan 21
+# python evaluation/eval-lsr-dtu.py --scan 21 --cam ../data/DTU/scan21/cameras.npz --data ../exps/dtu-rend-d/21/2023_03_06_11_08_27/wireframes/latest-DznELm3K-wfi_checked.npz
+
+# #scan 22
+# python evaluation/eval-lsr-dtu.py --scan 22 --cam ../data/DTU/scan22/cameras.npz --data ../exps/dtu-rend-d/22/2023_03_06_18_46_24/wireframes/latest-oTDlkn2H-wfi_checked.npz
+
+# #scan 23
+# python evaluation/eval-lsr-dtu.py --scan 23 --cam ../data/DTU/scan23/cameras.npz --data ../exps/dtu-rend-d/23/2023_03_06_18_45_30/wireframes/latest-H3In0MGp-wfi_checked.npz
+
